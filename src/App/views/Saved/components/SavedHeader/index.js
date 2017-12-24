b4ec4408b0fad6e619983690bc3875a6f8168d37
hajji tarek <hajji-tarik@outlook.fr>
2017-12-24 01:22:38 +0000

    fix issues

--- src/App/views/Saved/components/SavedHeader/index.js
+++ src/App/views/Saved/components/SavedHeader/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import { isEmpty, omit } from 'lodash';
 import LinearGradient from 'react-native-linear-gradient';
 import { getFromStorage } from '../../../../utils/cacheManager';
 import colors from '../../../../../design/index';
 import CONSTANTS from '../../../../config/appConstants';
+import styles from './index.style';
 
 export default class extends Component {
   constructor(props) {
@@ -84,42 +85,3 @@ export default class extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  optionMenu: {
-    paddingBottom: 5,
-    paddingLeft: 10,
-    paddingRight: 10,
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
-  },
-  removeAll: {
-    flex: 1,
-    flexDirection: 'row'
-  },
-  removeAllIcon: {
-    flexDirection: 'row',
-    alignItems: 'flex-end',
-    width: 40,
-    paddingLeft: 0
-  },
-  removeAllText: {
-    color: '#fff',
-    fontWeight: 'bold',
-    paddingTop: 6,
-    fontFamily: 'AlegreyaSans-Medium',
-    fontSize: 15
-  },
-  selectArticle: {
-    flexDirection: 'row',
-    color: '#fff',
-    paddingTop: 6,
-    fontWeight: 'bold',
-    alignItems: 'flex-start',
-    fontFamily: 'AlegreyaSans-Medium',
-    paddingRight: 15,
-    fontSize: 15
-  },
-  gradient: {
-    ...StyleSheet.absoluteFillObject
-  },
-});
